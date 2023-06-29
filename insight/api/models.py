a619e74cb785e1142e854da000d16f2465df26d9
Bentley Hensel <bentley.hensel@civicactions.com>
2023-06-28 23:29:08 -0400

    Adding orgs.entities Model

--- insight/api/models.py
+++ insight/api/models.py
@@ -14,6 +14,20 @@ class Domain(Base):
     org_id = Column(Integer, ForeignKey('orgs.entities.id'))  # Corresponding Entity ID
     urls = relationship('Url', back_populates='domain')  # Relationship to urls
 
+
+class Entity(Base):
+    __tablename__ = "entities"
+
+    id = Column(Integer, primary_key=True, index=True)  # Use sequence in PostgreSQL, like: id = Column(Integer, Sequence('fed_agencies_id_seq'), primary_key=True,)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())  # When added
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())  # When row updated
+    name = Column(String, unique=True)  # Entity name
+    type = Column(String, ForeignKey('refs.org_types.name'))  # What type of org is this? From refs.org_types
+    acronym = Column(String)  # Entity acronym
+    active = Column(Boolean, default=False)  # Is this entity active?
+    domains = relationship('Domain', back_populates='org')
+
+
 class Url(Base):
     __tablename__ = 'urls'
 
