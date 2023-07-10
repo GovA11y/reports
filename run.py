f0aa048249b63a0ec2331d860eb9b752ace22b14
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-10 14:15:39 -0400

    Expose proper app port

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # run.py
 
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    # Use the APP_PORT environment variable
+    port = os.environ.get('APP_PORT', 8080)
+    app.run(host='0.0.0.0', port=port, debug=True)
