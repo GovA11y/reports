1228432d92f1690db911944fa979882f8a672ef3
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-11 18:40:28 -0400

    Dynamic rule_type & tested_from|to

---
+++ app/api/database/clickhouse/queries/axe/current_violations_time.sql
@@ -0,0 +1,9 @@
+-- Get Curent Violations with timefilter
+-- Relative Path: app/api/database/clickhouse/queries/axe/current_violations_time.sql
+SELECT *
+FROM gova11y.axe_tests_recent
+WHERE domain = '%s'
+  AND rule_type IN (%s)
+  AND tested_at BETWEEN '%s' AND '%s'
+ORDER BY tested_at DESC
+LIMIT %s;
