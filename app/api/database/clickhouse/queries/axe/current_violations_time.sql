a89cc177b8d54561a74cac5aaecdbe01b0609690
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-12 14:11:30 -0400

    Changed current violations domain from = to ILIKE

--- app/api/database/clickhouse/queries/axe/current_violations_time.sql
+++ app/api/database/clickhouse/queries/axe/current_violations_time.sql
@@ -2,7 +2,7 @@
 -- Relative Path: app/api/database/clickhouse/queries/axe/current_violations_time.sql
 SELECT *
 FROM gova11y.axe_tests_recent
-WHERE domain = '%s'
+WHERE domain ILIKE '%s'
   AND rule_type IN (%s)
   AND tested_at BETWEEN '%s' AND '%s'
 ORDER BY tested_at DESC
