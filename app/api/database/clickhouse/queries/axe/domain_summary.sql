ee3a878f9838ccc9c4fb2d98ac1abd1dcba64125
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-17 14:58:26 -0400

    Updated Domain Summary

--- app/api/database/clickhouse/queries/axe/domain_summary.sql
+++ app/api/database/clickhouse/queries/axe/domain_summary.sql
@@ -1,20 +1,14 @@
-# Relative Path: app/api/database/clickhouse/queries/axe/domain_summary.sql
+-- Relative Path: app/api/database/clickhouse/queries/axe/domain_summary.sql
 -- Domain Filter with URL Count
 SELECT
-    at.domain AS "domain_name",
-    COUNT(DISTINCT at.url_id) AS "urls_testes",
-    COUNTIf(at.rule_type = 'passes') AS "count_passes",
-    COUNTIf(at.rule_type = 'violations') AS "count_violations",
-    COUNTIf(at.rule_type = 'incompatible') AS "count_incompatibles",
+    "domain" AS "domain_name",
+    COUNT(DISTINCT url) AS "urls_tested",
+    COUNTIf(rule_type = 'passes') AS "count_passes",
+    COUNTIf(rule_type = 'violations') AS "count_violations",
+    COUNTIf(rule_type = 'inapplicable') AS "count_inapplicable",
     COUNT(*) AS "count_recent_tests"
 FROM
-    axe_tests at
-INNER JOIN
-    (
-    SELECT url_id, axe_id, target, max(tested_at) as max_tested_at
-    FROM axe_tests
-    GROUP BY url_id, axe_id, target
-    ) sub
-ON at.url_id=sub.url_id AND at.axe_id=sub.axe_id AND at.target=sub.target AND at.tested_at=sub.max_tested_at
-WHERE at.domain LIKE '%s'
-GROUP BY at.domain;
+    axe_tests_recent atr
+WHERE "domain" LIKE '%s'
+GROUP BY "domain"
+ORDER BY "count_recent_tests" desc;
