f9ba07d81a1179cef6794c3aee04c82692d55562
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-19 14:02:02 -0400

    Additional Domain Summary Fields

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 # app/__init__.py
-
+import os
+from dotenv import load_dotenv
+import pyroscope
 from flask import Flask
 from app.api.domain.views import domain_bp
 from app.api.axe.views import axe_bp
 from app.api.activity.views import activity_bp
 from app.api.metrics.views import metrics_bp
+from app.api.utils import start_scheduled_jobs
+from app.logging import logger
+
+load_dotenv()
 
 
 def create_app():
@@ -17,3 +23,19 @@ def create_app():
     app.register_blueprint(metrics_bp)
 
     return app
+
+
+def configure_pyroscope():
+    # Configure Pyroscope
+    pyroscope.configure(
+        application_name=os.getenv("PYROSCOPE_APPLICATION_NAME"),
+        server_address=os.getenv("PYROSCOPE_SERVER"),
+    )
+
+
+
+
+
+def startup():
+    start_scheduled_jobs()
+    configure_pyroscope()
--- app/api/database/postgres/queries/domain/summary.sql
+++ app/api/database/postgres/queries/domain/summary.sql
@@ -1,9 +1,16 @@
 -- Relative Path: app/api/database/postgres/queries/domain/summary.sql
 -- Selects the domain and subdomain of the search term along with how many urls each domain has
+-- Get Domain Active Status
 SELECT
     targets.domains.id AS "domain_id",
     targets.domains.domain,
-    COUNT(targets.urls.url) AS "url_count"
+    COUNT(targets.urls.url) AS "url_count",
+    SUM(targets.urls.is_objective::int) AS "objective_urls",
+    SUM(targets.urls.active_scan_axe::int) AS "active_scan_urls_axe",
+    SUM(targets.urls.active_main::int) AS "active_urls",
+    SUM(targets.urls.errored::int) AS "errored_urls",
+    targets.domains.active as "is_domain_active",
+    targets.domains.is_objective as "is_domain_objective"
 FROM
     targets.domains
 JOIN
