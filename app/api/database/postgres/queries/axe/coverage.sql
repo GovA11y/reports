39f06d5fb6bc9cb4a74367aa9094f8a6164ee2f4
Bentley Hensel <bentleyhensel@gmail.com>
2023-09-26 11:44:28 -0400

    Domain Coverage Report
    Hotfix for #15
    
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

--- app/api/database/postgres/queries/axe/coverage.sql
+++ app/api/database/postgres/queries/axe/coverage.sql
@@ -7,13 +7,17 @@ SELECT
     COUNT(CASE WHEN u.active_scan_axe THEN 1 END) AS active_axe_urls,
     COUNT(CASE WHEN u.is_objective THEN 1 END) AS is_objective_urls,
     COUNT(CASE WHEN u.errored THEN 1 END) AS errored_urls,
+    COUNT(DISTINCT sd.url_id) AS axe_tested_count,
     ROUND(
         100.0 * COUNT(CASE WHEN u.active_scan_axe THEN 1 END) / NULLIF(COUNT(u.id), 0),
         2
     ) AS domain_coverage_axe
 FROM targets.domains d
 LEFT JOIN targets.urls u ON d.id = u.domain_id
-WHERE d."domain" ILIKE '%s'
+LEFT JOIN axe.scan_data sd ON u.id = sd.url_id
+WHERE d."domain" LIKE '%s'
 GROUP BY d.domain
 ORDER BY d.domain;
+
+
 
